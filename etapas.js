aa99c9d3e3e2bebdd4ed13c262b4c24101b63290
Marcia <marcia.o.t.melo@gmail.com>
2022-08-13 22:33:11 -0300

    Projeto Urna Eletrônica
    Feito em JavaScript, HTML e CSS.

---
+++ etapas.js
@@ -0,0 +1,69 @@
+let etapas = [
+{
+    titulo:'VEREADOR (A)',
+    numeros: 5,
+    candidatos: [
+        {
+        numero: 15555,
+        nome: 'Cicrano de tal',
+        partido: 'PPP',
+        fotos: [
+        {url:'15555.jpg', legenda: 'Vereador'}
+        ]
+    },
+    {
+        numero: 18567,
+        nome: 'Maria',
+        partido: 'PDT',
+        fotos: [
+            {url:'18567.jpg', legenda: 'Vereador'}
+            ]
+    },
+    {
+        numero: 77222,
+        nome: 'Cicrano de tal',
+        partido: 'PPP',
+        fotos: [
+            {url:'77222.jpg', legenda: 'Vereador'}
+            ]
+    },
+    {
+            numero: 12543,
+            nome: 'Itache',
+            partido: 'DMD',
+            fotos: [
+                {url:'12543.jpg', legenda: 'Vereador'}
+                ]
+    },
+        
+    ]
+},
+{
+    titulo:'PREFEITO (A)',
+    numeros: 2,
+    candidatos: [
+{
+    numero: 15,
+    nome: 'Cris Macedo',
+    partido: 'PMDB',
+    vice: 'Melinda',
+    fotos: [
+        {url:'15.jpg', legenda:'Prefeito'},
+        {url:'22.jpg', legenda: 'Vice-Prefeito', small: true}
+    ]
+},
+{
+    
+        numero: 85,
+        nome: 'Keny Martins',
+        partido: 'PSDB',
+        vice: 'Linda Silva',
+        fotos: [
+            {url:'85.jpg', legenda:'Prefeito'},
+            {url:'50.jpg', legenda: 'Vice-Prefeito', small: true}
+            ]   
+        },
+    
+    ]
+}
+];
